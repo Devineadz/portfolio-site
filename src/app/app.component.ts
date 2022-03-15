c2bcffc9cbe14b085d8c7e65eca40781e9c54068
Diana Zitting-Rioux <44038147+Devineadz@users.noreply.github.com>
2022-03-15 12:20:52 -0400

    Google analytics example commented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import * as AOS from 'aos';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,20 @@ import * as AOS from 'aos';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  /* Google Analytics, add your own tag number
+  constructor(private router: Router){
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+          gtag('config', 'YOUR-TAG', 
+                {
+                  'page_path': event.urlAfterRedirects
+                }
+               );
+       }
+    }
+    )
+  }*/
 
   ngOnInit() {
     AOS.init();
